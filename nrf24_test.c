ca36e9c1fd4fd3bc2039215b6570a5349c7b9db3
Sergey Matyukevich <geomatsi@gmail.com>
2015-05-03 23:37:23 +0300

    Add command line options for nrf24_test
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- nrf24_test.c
+++ nrf24_test.c
@@ -58,9 +58,9 @@ void radio_gpio_setup(void)
 	pcduino_gpio_write(PCDUINO_RFM69_CSN_NAME, 1);
 }
 
-void radio_spi_setup(void)
+void radio_spi_setup(char *name)
 {
-	pcduino_spi_open(PCDUINO_NRF24_SPI);
+	pcduino_spi_open(name);
 	pcduino_spi_info();
 	pcduino_spi_init();
 	pcduino_spi_info();
@@ -99,17 +99,18 @@ struct rf24 nrf = {
 
 /* */
 
-struct rf24* radio_init(void)
+void nrf24_test_usage(char *name)
 {
-	radio_gpio_setup();
-	radio_spi_setup();
-
-	rf24_init(&nrf);
-
-	return &nrf;
+	printf("usage: %s [-h] -d <spidev>\n", name);
+	printf("\t-h, --help			this help message\n");
+	printf("\t-d, --device <spidev>		spidev for nRF24L01, default is '/dev/spidev0.0\n");
+	printf("\t--peer1 <addr>			not yet supported\n");
+	printf("\t--peer2 <addr>			not yet supported\n");
 }
 
-int main(void)
+/* */
+
+int main(int argc, char *argv[])
 {
 	uint8_t addr0[] = {'E', 'F', 'C', 'L', 'I'};
 	uint8_t addr1[] = {'E', 'F', 'S', 'N', '1'};
@@ -119,36 +120,82 @@ int main(void)
 	uint32_t more_data;
 	uint8_t pipe;
 
-	struct rf24 *nrf;
+	struct rf24 *pnrf;
+
+	/* command line options */
+
+	char *spidev_name = PCDUINO_NRF24_SPI;
+
+	char peer[2][6] = {
+			"EFCLI\0",
+			"EFSN1\0",
+	};
+
+	int opt;
+	const char opts[] = "d:h";
+    const struct option longopts[] = {
+        {"device", required_argument, NULL, 'd'},
+        {"help",   optional_argument, NULL, 'h'},
+        {"peer1",  required_argument, NULL, '0'},
+        {"peer2",  required_argument, NULL, '1'},
+        {NULL,}
+    };
+
+    while (opt = getopt_long(argc, argv, opts, longopts, &opt), opt > 0) {
+        switch (opt) {
+			case 'd':
+				spidev_name = strdup(optarg);
+				break;
+			case '0':
+				if (strlen(optarg) >= 5)
+					strncpy(peer[0], optarg, 5);
+				break;
+			case '1':
+				if (strlen(optarg) >= 5)
+					strncpy(peer[1], optarg, 5);
+				break;
+			case 'h':
+			default:
+				nrf24_test_usage(argv[0]);
+				exit(0);
+        }
+    }
+
+	/* setup SPI and GPIO */
+
+	radio_gpio_setup();
+	radio_spi_setup(spidev_name);
+
+	/* setup nRF24L01 */
+
+	rf24_init(&nrf);
+	pnrf = &nrf;
+
+	rf24_print_status(rf24_get_status(pnrf));
 
 	/* */
 
-	nrf = radio_init();
-	rf24_print_status(rf24_get_status(nrf));
+	rf24_set_payload_size(pnrf, sizeof(buf));
 
-	/* */
+	rf24_open_reading_pipe(pnrf, 0x0 /* pipe number */, addr0);
+	rf24_open_reading_pipe(pnrf, 0x1 /* pipe number */, addr1);
 
-	rf24_set_payload_size(nrf, sizeof(buf));
-
-	rf24_open_reading_pipe(nrf, 0x0 /* pipe number */, addr0);
-	rf24_open_reading_pipe(nrf, 0x1 /* pipe number */, addr1);
-
-	rf24_start_listening(nrf);
-	rf24_print_status(rf24_get_status(nrf));
-    rf24_print_details(nrf);
+	rf24_start_listening(pnrf);
+	rf24_print_status(rf24_get_status(pnrf));
+    rf24_print_details(pnrf);
 
 	/* */
 
 	while (1) {
 
-		if (rf24_available(nrf, &pipe)) {
+		if (rf24_available(pnrf, &pipe)) {
 
 			if ((pipe < 0) || (pipe > 5)) {
 				printf("WARN: invalid pipe number 0x%02x\n", (int) pipe);
 			} else {
 				printf("INFO: data ready in pipe 0x%02x\n", pipe);
 				memset(buf, 0x0, sizeof(buf));
-				more_data = rf24_read(nrf, buf, sizeof(buf));
+				more_data = rf24_read(pnrf, buf, sizeof(buf));
 				printf("INFO: data [%s]\n", buf);
 				if (!more_data)
 					printf("WARN: RX_FIFO not empty: %d\n", more_data);
