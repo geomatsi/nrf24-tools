5f6981c3eeac979f3c66f48ce8e55588650bfb81
Sergey Matyukevich <geomatsi@gmail.com>
2015-10-17 15:29:43 +0300

    Minor update: improve x axis date ticks and add legends
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scripts/data_plotter.py
+++ scripts/data_plotter.py
@@ -1,46 +1,81 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plot
+import matplotlib.dates as mdates
 import numpy as np
+import datetime
 
 ### main
-
-# setup figure size: NB inches
-plot.figure(figsize = (15, 10))
 
 # read data
 
 with open("t.txt") as f:
     data = f.read()
 
-data = data.split('\n')
+data = data.split('\n') #[0:2000]
 
 x = [float(row.split(';')[0]) for row in data if len(row) > 0]
 y = [float(row.split(';')[1]) for row in data if len(row) > 0]
+
+# threshold
+T = 1000
+
+# convert epoch to date format
+x = map(datetime.datetime.fromtimestamp, x)
+
+# cleanup
+plot.close('all')
+
+# setup figure size: NB inches
+fig = plot.figure(figsize = (15, 10))
+fig.canvas.set_window_title('Gas sensor')
 
 # plot 1: gas level
 
 y1 = y
 x1 = x
 
-g1 = plot.subplot(211)
-g1.set_title("Gas level")
-g1.set_xlabel("time")
-g1.set_ylabel("gas")
+a1 = fig.add_subplot(211)
 
-g1.plot(x1, y1, c='r', label='gas level')
+a1.set_ylabel("adc")
+a1.grid(True)
 
-# plot 2: sliding avg
+a1.plot(x1, y1, c='r')
+a1.legend(['gas level'])
+
+a1.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m %H:%M'))
+a1.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+
+for tick in a1.get_xticklabels():
+	tick.set_rotation(20)
+	tick.set_size('x-small')
+
+# plot 2.1: sliding avg
 
 y1 = np.convolve(y, np.ones((20,))/20, mode='valid')
 x1 = x[0:len(y1)]
 
-g2 = plot.subplot(212)
-g2.set_title("Sliding avg")
-g2.set_xlabel("time")
-g2.set_ylabel("avg")
+a2 = fig.add_subplot(212)
 
-g2.plot(x1, y1, c='g', label='avg')
+a2.set_ylabel("avg")
+a2.grid(True)
 
-# draw new data
+a2.plot(x1, y1, c='g')
+
+a2.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m %H:%M'))
+a2.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+
+for tick in a2.get_xticklabels():
+	tick.set_rotation(20)
+	tick.set_size('x-small')
+
+# plot 2.2: threshold for sliding avg
+
+y2 = [T if (t > T) else 0.0 for t in y1]
+x2 = x1
+
+a2.plot(x2, y2, c='b')
+a2.legend(['sliding avg window', "threshold (" + str(T) + ")"])
+
+# draw data
 plot.show()
