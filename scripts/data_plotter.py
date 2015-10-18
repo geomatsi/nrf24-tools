c9b40e429cb3ef96f28bcb81fed7f467e976b5b9
Sergey Matyukevich <geomatsi@gmail.com>
2015-10-18 12:28:16 +0300

    Update data plotter: adaptive min interval for date locator
    - adaptive min interval for date locator
      calculate min interval to make sure that we have 10 x-axis ticks
    
    - minor fixes: legend notes and variable names
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scripts/data_plotter.py
+++ scripts/data_plotter.py
@@ -17,11 +17,14 @@ data = data.split('\n') #[0:2000]
 x = [float(row.split(';')[0]) for row in data if len(row) > 0]
 y = [float(row.split(';')[1]) for row in data if len(row) > 0]
 
-# threshold
-T = 1000
+# x-axis: calculate minute interval to have 10 x-axis marks
+mint = int((x[len(x) - 1] - x[0])/60/10)
 
-# convert epoch to date format
+# x-axis: convert epoch to date format
 x = map(datetime.datetime.fromtimestamp, x)
+
+# y-axis: threshold
+thr = 1000
 
 # cleanup
 plot.close('all')
@@ -44,38 +47,38 @@ a1.plot(x1, y1, c='r')
 a1.legend(['gas level'])
 
 a1.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m %H:%M'))
-a1.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+a1.xaxis.set_major_locator(mdates.MinuteLocator(interval=mint))
 
 for tick in a1.get_xticklabels():
 	tick.set_rotation(20)
 	tick.set_size('x-small')
 
-# plot 2.1: sliding avg
+# plot 2.1: moving average
 
 y1 = np.convolve(y, np.ones((20,))/20, mode='valid')
 x1 = x[0:len(y1)]
 
 a2 = fig.add_subplot(212)
 
-a2.set_ylabel("avg")
+a2.set_ylabel("average")
 a2.grid(True)
 
 a2.plot(x1, y1, c='g')
 
 a2.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m %H:%M'))
-a2.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+a2.xaxis.set_major_locator(mdates.MinuteLocator(interval=mint))
 
 for tick in a2.get_xticklabels():
 	tick.set_rotation(20)
 	tick.set_size('x-small')
 
-# plot 2.2: threshold for sliding avg
+# plot 2.2: threshold crosses by moving average
 
-y2 = [T if (t > T) else 0.0 for t in y1]
+y2 = [thr if (t > thr) else 0.0 for t in y1]
 x2 = x1
 
 a2.plot(x2, y2, c='b')
-a2.legend(['sliding avg window', "threshold (" + str(T) + ")"])
+a2.legend(['moving average', "threshold (" + str(thr) + ")"])
 
 # draw data
 plot.show()
